fbe64eba5393e25edf4ad020fadee56c00d8ac92
krishpranav <krisna.pranav@gmail.com>
2021-04-20 11:37:45 +0530

    updated

---
+++ example/rules/custom-rule.js
@@ -0,0 +1,14 @@
+/**
+ * Initiator method
+ *
+ * @param   {Object}  data  Data object with request values
+ * @return  void
+ */
+
+exports.fire = ( data ) => {
+
+    console.log( 'Custom wpcheck rule is fired' )
+
+    console.log( data )
+
+}
